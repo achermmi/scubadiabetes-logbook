c500c600b04ef6d9477d35b6959c08975b52bdad
Mirko Achermann <mirko.achermann@gmail.com>
2026-05-11 15:04:39 +0200

    fix disclaimer

--- templates/predive-check.php
+++ templates/predive-check.php
@@ -109,7 +109,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<line x1="12" y1="8" x2="12" y2="12"/>
 			<line x1="12" y1="16" x2="12.01" y2="16"/>
 		</svg>
-		<?php esc_html_e( 'Questo strumento è un supporto decisionale e non sostituisce il giudizio clinico del tuo medico. Segui sempre le indicazioni del protocollo Diabete Sommerso.', 'sd-logbook' ); ?>
+		<?php esc_html_e( 'Questo strumento è un supporto decisionale e non sostituisce il giudizio clinico del tuo medico.', 'sd-logbook' ); ?><br>
+		<?php esc_html_e( 'Segui sempre le indicazioni del protocollo Diabete Sommerso.', 'sd-logbook' ); ?>
 	</p>
 
 </div><!-- .sd-predive-wrap -->
