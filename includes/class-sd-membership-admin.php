edb57087cf3bb84772266d120760de29f0520b97
Mirko Achermann <mirko.achermann@gmail.com>
2026-05-29 17:03:01 +0200

    mail hhmmss

--- includes/class-sd-membership-admin.php
+++ includes/class-sd-membership-admin.php
@@ -2054,7 +2054,7 @@ class SD_Membership_Admin {
 		$first    = sanitize_file_name( (string) ( $member->first_name ?? '' ) );
 		$last     = sanitize_file_name( (string) ( $member->last_name ?? '' ) );
 		$label    = trim( $first . '-' . $last, '-' ) ?: ( 'socio-' . (int) $member->id );
-		$pdf_path = get_temp_dir() . $label . '-' . gmdate( 'Ymd' ) . '-' . substr( md5( uniqid( '', true ) ), 0, 6 ) . '.pdf';
+		$pdf_path = get_temp_dir() . $label . '-' . gmdate( 'Ymd' ) . '-' . gmdate( 'His' ) . '.pdf';
 		file_put_contents( $pdf_path, $pdf_str ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_file_put_contents
 		return array( $pdf_path );
 	}
